a2c562f4f12edbd46491048b078e917565e770c0
Noémie Mateu <noemiemateu@yahoo.fr>
2023-10-13 15:19:09 +0200

    Brief completed

--- Cli.java
+++ Cli.java
@@ -1,4 +1,9 @@
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Map;
+
 
 public class Cli {
 
@@ -10,6 +15,65 @@ public class Cli {
 			String output = ""; // A variable named output of type String
 			if (command.equals("exit")) {
 				break; // Forces exit of the while loop
+			}
+			if (command.equals("date")) {
+				LocalDate myObj = LocalDate.now(); // Create a date object
+				System.out.println(myObj); // Display the current date
+			}
+			if (command.equals("time")) {
+				LocalTime myObj = LocalTime.now(); // Create a time object
+				System.out.println(myObj); // Display the current time
+			}
+			if (command.equals("datetime")) {
+				LocalDateTime myObj = LocalDateTime.now(); // Create a date time object
+				System.out.println(myObj); // Display the current date time
+			}
+
+			if (command.equals("useraccount")) {
+
+			 	
+				String userName = System.getProperty("user.name"); // getProperty use different arguments
+				System.out.println(userName);
+			}
+
+			if (command.equals("userhome")) {
+
+				String userHomeDirectory = System.getProperty("user.home");
+				System.out.println(userHomeDirectory);
+			}
+
+			if (command.equals("os")) {
+
+				String osName = System.getProperty("os.name"); // return operating system name
+				String osVersion = System.getProperty("os.version"); // return operating system version
+				System.out.println(osName + " (" + osVersion + ") ");
+			
+			}
+			
+			if (command.equals("printenv")){
+
+				System.out.print("Please enter the name of the environment variable: ");
+				String variableToLookFor = scanner.nextLine(); // Thanks to the scanner, the name of the input variable can be retrieved
+				
+				Map<String, String> variablesEnv = System.getenv();  //System.getenv() returns a map that associates environment variable names (strings) with their corresponding values (also strings).
+		
+				if (variablesEnv.containsKey(variableToLookFor)) { //containsKey to check if the key (variable name) entered by the user exists in the Map (Map is key value collection )
+					String value = variablesEnv.get(variableToLookFor);
+					System.out.println("The name of the environment variable " + variableToLookFor + " is : " + value);
+				} else {
+					System.out.println("The environment variable " + variableToLookFor + " was not found.");
+				}
+			}
+
+			if (command.equals("echo")){
+				
+				System.out.print("Please enter your text :");
+				String userText = scanner.nextLine();
+
+				System.out.println(userText);
+        
+				
+		
 			} else {
 				// String concatenation
 				output = "Command '" + command + "' not found.";
